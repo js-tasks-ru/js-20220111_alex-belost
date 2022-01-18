92ba41fd1df2e3a1ebe8c21e44955e6a6d9bf04e
alex belost <alex.belost@gmail.com>
2022-01-18 21:16:49 +0200

    [03-objects-arrays-intro-to-testing] 1 Create getter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,9 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const propertyAccessPath = path.split('.');
 
+  return (object) => {
+    return propertyAccessPath.reduce((result, key) => (result && result[ key ]), object);
+  };
 }
