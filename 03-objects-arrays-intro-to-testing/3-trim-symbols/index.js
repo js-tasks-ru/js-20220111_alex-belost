0709de8913e4404fa43e353a1f35178dabf73c22
alex belost <alex.belost@gmail.com>
2022-01-19 18:10:22 +0200

    [03-objects-arrays-intro-to-testing] 3 Trim symbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,24 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  let iterableLetter = null;
+  let counter = 0;
 
+  let result = '';
+
+  for (const letter of string) {
+    if (iterableLetter !== letter) {
+      iterableLetter = letter;
+      counter = 0;
+    }
+
+    if (counter >= size) {
+      continue;
+    }
+
+    result += iterableLetter || '';
+    counter++;
+  }
+
+  return result;
 }
